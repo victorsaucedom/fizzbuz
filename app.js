e8522634ea638df28ea97dba0aad196282416d2a
Victor Saucedo <victor.estadistica2012@gmail.com>
2022-04-27 17:59:02 -0500

    agregando app.js sin modulacion

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const fs = require("fs");
 const rawdata = fs.readFileSync("explorers.json");
 const explorers = JSON.parse(rawdata);
 
+console.log("Hola Atodos")
+
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+console.log(explorersInNode.length)
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
