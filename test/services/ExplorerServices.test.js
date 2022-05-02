35fac240823f7b99c59c58e3e8c935e6fd36126f
Victor Saucedo <victor.estadistica2012@gmail.com>
2022-05-02 17:10:03 -0500

    Actualizando y agregando Test

--- test/services/ExplorerServices.test.js
+++ test/services/ExplorerServices.test.js
@@ -1,8 +1,21 @@
-const Explorerservices = require('./../test/services');
+const Reader = require("./../../lib/utils/reader");
+const explorers = Reader.readJsonFile("explorers.json");
+const ExplorerService = require("./../../lib/services/explorerService");
 
-describe("Test para ExplorerService", () => {
-    test('1) Create a mission commander objet', () => {
-    const myMissionCommander = new MissionCommander("Woopa")
-    expect(myMissionCommander.name).toBe("Woopa");
+describe("Unit test for ExplorerService class", () => {
+
+    test("Filter explorers in node mission", () => {
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode[0].mission).toBe("node");
     });
-  })
+
+    test("Get amount of explorers by mission", () => {
+        const amountOfExplorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(amountOfExplorersInNode).toBe(10);
+    });
+
+    test("Get explorers username by mission", () => {
+        const usernamesInNode = ExplorerService.getExporersUsernameByMission(explorers, "node");
+        expect(usernamesInNode[0]).toBe("ajolonauta1");
+    });
+});
